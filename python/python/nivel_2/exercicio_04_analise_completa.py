ed33eea66320828bb064880662f46837a27f2531
Guilherme <guiguizaovial@gmail.com>
2026-02-11 09:35:36 -0300

    Exercicio_04_analise_completa.py

---
+++ python/python/nivel_2/exercicio_04_analise_completa.py
@@ -0,0 +1,13 @@
+numero = int(input("Digite um numero:"))
+
+if numero > 0:
+    print("Ele é positivo")
+elif numero < 0:
+    print("Ele é negativo")
+else:
+    print("Ele é zero")
+
+if numero % 2 == 0:
+    print("Ele é par")
+else:
+    print("Ele é impar")
