c2b26a98daf765f1509ffb3f19c88b8aa387929b
Guilherme <guiguizaovial@gmail.com>
2026-03-20 14:30:18 -0300

    Python_Positivo_e_Negativo.py

---
+++ python/Python_Positivo_e_Negativo.py
@@ -0,0 +1,12 @@
+# Verificar se número é positivo ou negativo
+
+numero = int(input("Digite um número: "))
+
+if numero > 0:
+    print("Número positivo")
+elif numero < 0:
+    print("Número negativo")
+else:
+    print("Número zero")
+
+
